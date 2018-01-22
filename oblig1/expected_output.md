f34ab55053691b68fe8ee46dea7aaea8f8dc5666
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2018-01-22 14:21:36 +0100

    added potential solution to oblig 1

---
+++ oblig1/expected_output.md
@@ -0,0 +1,23 @@
+# Forventet utskrift
+
+Prosesser på samme linje kjører i vilkårlig rekkefølge. (Skjer Samtidig).
+
+```
+Prosess 0 kjører
+Prosess 2 kjører
+
+Prosess 0 kjørte i 1 sekunder
+Prosess 1 kjører
+Prosess 4 kjører
+
+Prosess 1 kjørte i 2 sekunder
+Prosess 2 kjørte i 3 sekunder
+Prosess 3 kjører
+
+Prosess 4 kjørte i 3
+Prosess 5 kjører
+
+Prosess 3 kjørte i 2 sekunder
+
+Prosess 5 kjørte i 3 sekunder
+```
---
+++ oblig1/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* exit */
+#include <unistd.h>     /* fork */
+#include <sys/wait.h>   /* waitpid */
+#include <sys/types.h>  /* pid_t */
+
+
+// Skriv ut number og sleep i time sekunder (simuler arbeid)
+void process(int number, int time) {
+    printf("Prosess %d kjører\n", number);
+    sleep(time);
+    printf("Prosess %d kjørte i %d sekunder\n", number, time);
+}
+
+int main(void) {
+
+    // lagrer pid til original tråd
+    int originalpid = getpid();
+
+    // variabel for piden til hver av trådene
+    int pid0, pid1, pid2, pid3, pid4, pid5;
+
+    // fork frem trådene (BARE I ORIGINAL TRÅDEN)
+    if (getpid() == originalpid) pid0 = fork();
+    if (getpid() == originalpid) pid1 = fork();
+    if (getpid() == originalpid) pid2 = fork();
+    if (getpid() == originalpid) pid3 = fork();
+    if (getpid() == originalpid) pid4 = fork();
+    if (getpid() == originalpid) pid5 = fork();
+
+    // Oppgavene hver av trådene skal gjøre. Tråd x vil gjøre arbiedet som står
+    //  under pidx == 0. Bruker sleep på å vente på tur
+    if (pid0 == 0) {
+        process(0, 1);
+    } else if (pid1 == 0) {
+        sleep(1);
+        process(1, 2);
+    } else if (pid2 == 0) {
+        process(2, 3);
+    } else if (pid3 == 0) {
+        sleep(3);
+        process(3, 2);
+    } else if (pid4 == 0) {
+        sleep(1);
+        process(4, 3);
+    } else if (pid5 == 0) {
+        sleep(4);
+        process(5, 3);
+    } else {
+        sleep(7);
+    }
+
+    return 0;
+}
